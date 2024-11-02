f58694d7ccf3d83e716f404809e07a5185424191
Mateus Matos de Oliveira <mateus.m.oliveira2303@gmail.com>
2024-11-01 23:57:44 -0300

    finalizado websockets

--- WebSockets/payments/pix.py
+++ WebSockets/payments/pix.py
@@ -7,12 +7,12 @@ class Pix:
         pass
 
     def create_payment(self):
-        bank_payment_id = uuid.uuid4()
+        bank_payment_id = str(uuid.uuid4())
 
         hash_payment = f'hash_payment_{bank_payment_id}'
 
         qr_code = qrcode.make(hash_payment)
-        qr_code.save(f'static\qrcode\qr_code_payment_{bank_payment_id}.png')
+        qr_code.save(f'static\img\qr_code_payment_{bank_payment_id}.png')
 
         return {
             'bank_payment_id': bank_payment_id,
